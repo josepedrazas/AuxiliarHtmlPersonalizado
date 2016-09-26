e1e9a6c976056705286ee2c1c72459c076cf0567
Jose Pedraza <josepe.pedraza@gmail.com>
2016-09-26 15:42:41 -0400

    Se creo la clase con los helpers,  se creo un formulario en la vista

---
+++ AuxiliarHtmlPersonalizado/Helpers/BootstrapHelpers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AuxiliarHtmlPersonalizado.Helpers
+{
+    public static class BootstrapHelpers
+    {
+        public static MvcHtmlString Etiqueta(this HtmlHelper helper,string target,string text)
+        {
+            var etiqeta = String.Format("<label for='{0}'>{1}</label>", target, text);
+            return MvcHtmlString.Create(etiqeta);
+        }
+        public static MvcHtmlString Boton(this HtmlHelper helper, string style,string text)
+        {
+            var boton = String.Format("<button type='submit' class='{0}'>{1}</button>", style, text);
+            return MvcHtmlString.Create(boton);
+        }
+    }
+}
